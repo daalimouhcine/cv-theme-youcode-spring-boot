13e79af3a431ae670c9b17105ccb6b633931cd29
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-20 23:05:55 +0100

    build(project): initialize the project with sprint initializer

---
+++ src/main/java/com/example/cvtheme/CvThemeYoucodeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.cvtheme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvThemeYoucodeSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvThemeYoucodeSpringBootApplication.class, args);
+    }
+
+}
