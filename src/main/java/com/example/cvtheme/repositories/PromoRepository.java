da2e4341f3b889c49695ca034cf4b13be3235672
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-22 23:25:29 +0100

    build(project): create promo repository interface

---
+++ src/main/java/com/example/cvtheme/repositories/PromoRepository.java
@@ -0,0 +1,8 @@
+package com.example.cvtheme.repositories;
+
+import com.example.cvtheme.entities.PromoEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PromoRepository extends CrudRepository<PromoEntity, Long> {
+
+}
