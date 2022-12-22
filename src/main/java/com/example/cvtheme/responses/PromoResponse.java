1e8266824cecf0f5bedeb2bf210ce8be35c93b9f
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-22 23:25:15 +0100

    build(project): create promo request class to store the response data

---
+++ src/main/java/com/example/cvtheme/responses/PromoResponse.java
@@ -0,0 +1,12 @@
+package com.example.cvtheme.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PromoResponse {
+    private String name;
+    private String year;
+    private String major;
+}
