ec0b49fed4233f33dd08f815e4636e3802396508
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-22 23:24:38 +0100

    build(project): create and impliment promo interface for service

---
+++ src/main/java/com/example/cvtheme/servicesImp/PromoServiceImp.java
@@ -0,0 +1,14 @@
+package com.example.cvtheme.servicesImp;
+
+import com.example.cvtheme.services.PromoService;
+import com.example.cvtheme.shared.dto.PromoDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PromoServiceImp implements PromoService {
+
+    @Override
+    public PromoDto createPromo(PromoDto promoDto) {
+        return null;
+    }
+}
