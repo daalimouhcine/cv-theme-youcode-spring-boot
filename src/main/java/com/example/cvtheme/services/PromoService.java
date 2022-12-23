b47e7caf918af8b771c3ae8095004b8d715648be
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-23 23:47:53 +0100

    build(project): modify promo service

--- src/main/java/com/example/cvtheme/services/PromoService.java
+++ src/main/java/com/example/cvtheme/services/PromoService.java
@@ -3,6 +3,9 @@ package com.example.cvtheme.services;
 import com.example.cvtheme.shared.dto.PromoDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 public interface PromoService {
-    PromoDto createPromo(PromoDto promoDto);
+    PromoDto createPromo(PromoDto promo);
+//    List<PromoDto>
 }
--- src/main/java/com/example/cvtheme/servicesImp/PromoServiceImp.java
+++ src/main/java/com/example/cvtheme/servicesImp/PromoServiceImp.java
@@ -1,14 +1,25 @@
 package com.example.cvtheme.servicesImp;
 
+import com.example.cvtheme.entities.PromoEntity;
+import com.example.cvtheme.repositories.PromoRepository;
 import com.example.cvtheme.services.PromoService;
 import com.example.cvtheme.shared.dto.PromoDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PromoServiceImp implements PromoService {
+    @Autowired
+    PromoRepository promoRepository;
 
     @Override
-    public PromoDto createPromo(PromoDto promoDto) {
-        return null;
+    public PromoDto createPromo(PromoDto promo) {
+        PromoEntity promoEntity = new PromoEntity();
+        BeanUtils.copyProperties(promo, promoEntity);
+        PromoEntity newPromo = promoRepository.save(promoEntity);
+        PromoDto promoDto = new PromoDto();
+        BeanUtils.copyProperties(newPromo, promoDto);
+        return promoDto;
     }
 }
