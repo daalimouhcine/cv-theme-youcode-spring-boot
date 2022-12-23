3e45d1c5b1540cef6fda90dc954a77827ad790e1
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-23 23:47:35 +0100

    build(project): modify promo repository

--- src/main/java/com/example/cvtheme/repositories/PromoRepository.java
+++ src/main/java/com/example/cvtheme/repositories/PromoRepository.java
@@ -2,7 +2,9 @@ package com.example.cvtheme.repositories;
 
 import com.example.cvtheme.entities.PromoEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PromoRepository extends CrudRepository<PromoEntity, Long> {
 
 }
