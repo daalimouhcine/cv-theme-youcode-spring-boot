77ce8eb6637f437c3b48ff21045792d1ca88eaf6
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-25 23:58:52 +0100

    build(project): add methods to get all promos and deleting theme

--- src/main/java/com/example/cvtheme/servicesImp/PromoServiceImp.java
+++ src/main/java/com/example/cvtheme/servicesImp/PromoServiceImp.java
@@ -8,6 +8,9 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class PromoServiceImp implements PromoService {
     @Autowired
@@ -21,5 +24,31 @@ public class PromoServiceImp implements PromoService {
         PromoDto promoDto = new PromoDto();
         BeanUtils.copyProperties(newPromo, promoDto);
         return promoDto;
+    }
+
+    @Override
+    public List<PromoDto> getPromos() {
+        List<PromoEntity> promoList =(List<PromoEntity>) promoRepository.findAll();
+        List<PromoDto> promoDtoList = new ArrayList<PromoDto>();
+        for(PromoEntity promoEntity: promoList) {
+            promoDtoList.add(new PromoDto());
+            BeanUtils.copyProperties(promoEntity, promoDtoList.get(promoDtoList.size()-1));
+        }
+        return promoDtoList;
+    }
+
+    @Override
+    public PromoDto getByReferenceName(String referenceName) {
+        PromoEntity promoEntity = promoRepository.findByReferenceName(referenceName);
+        PromoDto promoDto = new PromoDto();
+        BeanUtils.copyProperties(promoEntity, promoDto);
+        return promoDto;
+    }
+
+    @Override
+    public void deletePromo(String referenceName) {
+        PromoEntity promoEntity = promoRepository.findByReferenceName(referenceName);
+        if(promoEntity == null) throw new RuntimeException("Promo not found");
+        promoRepository.delete(promoEntity);
     }
 }
