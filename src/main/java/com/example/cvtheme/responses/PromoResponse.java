b3a10069ba6300717c16465bf9535408ff6e2a84
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-25 23:58:18 +0100

    build(project): make the response returning the referenceName

--- src/main/java/com/example/cvtheme/responses/PromoResponse.java
+++ src/main/java/com/example/cvtheme/responses/PromoResponse.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PromoResponse {
+    private String referenceName;
     private String name;
     private String year;
     private String major;
