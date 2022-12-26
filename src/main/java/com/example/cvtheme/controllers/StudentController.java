303faef7ddd46f9be11536ec4d3ac6aa179cb543
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-26 22:39:02 +0100

    build(project): modify controllers

---
+++ src/main/java/com/example/cvtheme/controllers/StudentController.java
@@ -0,0 +1,38 @@
+package com.example.cvtheme.controllers;
+
+import com.example.cvtheme.requests.StudentRequest;
+import com.example.cvtheme.responses.StudentResponse;
+import com.example.cvtheme.services.StudentService;
+import com.example.cvtheme.shared.dto.StudentDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+@CrossOrigin(origins = "*")
+public class StudentController {
+    @Autowired
+    StudentService studentService;
+
+    @GetMapping
+    public String getUsers() {
+        System.out.println("getUsers");
+        return "get users";
+    }
+
+    @PostMapping
+    public ResponseEntity<StudentResponse> createStudent(@RequestBody StudentRequest studentRequest) {
+        StudentDto studentDto = new StudentDto();
+        BeanUtils.copyProperties(studentRequest, studentDto);
+        StudentDto createStudent = studentService.createStudent(studentDto);
+        StudentResponse studentResponse = new StudentResponse();
+        BeanUtils.copyProperties(createStudent, studentResponse);
+        studentResponse.setStatus(createStudent.getStatus().name());
+//        studentResponse.setPromo(createStudent.getPromo());
+        BeanUtils.copyProperties(createStudent.getPromo(), studentResponse.getPromo());
+        return new ResponseEntity<StudentResponse>(studentResponse, HttpStatus.CREATED);
+    }
+}
