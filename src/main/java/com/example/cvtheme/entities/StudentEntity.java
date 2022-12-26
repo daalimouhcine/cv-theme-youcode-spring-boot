bcbb426656055358b434fc8cf01cd7674a78aa8f
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-26 22:38:25 +0100

    build(project): modify student entity

--- src/main/java/com/example/cvtheme/entities/StudentEntity.java
+++ src/main/java/com/example/cvtheme/entities/StudentEntity.java
@@ -11,6 +11,8 @@ import java.util.List;
 @Setter
 @Entity(name = "students")
 public class StudentEntity extends User {
+    @Column(nullable = false, unique = true)
+    private String studentReferenceName;
     @Column(nullable = false)
     private StudentStatus status;
     @ManyToOne
