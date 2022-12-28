0c8a4f9951fd9608440da3c69b3357f33b3124be
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-28 21:57:41 +0100

    build(project): update student service

--- src/main/java/com/example/cvtheme/servicesImp/StudentServiceImp.java
+++ src/main/java/com/example/cvtheme/servicesImp/StudentServiceImp.java
@@ -38,6 +38,5 @@ public class StudentServiceImp implements StudentService {
         StudentDto studentDto = new StudentDto();
         BeanUtils.copyProperties(newStudent, studentDto);
         return studentDto;
-
     }
 }
